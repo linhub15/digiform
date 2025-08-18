a846afe1b0cc9dace8360a355cd99096b897789f
Hubert Lin <linhub15@users.noreply.github.com>
2025-08-18 00:17:04 -0600

    feat: build for cloudflare

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
 import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
-import { TanStackDevtools } from "@tanstack/react-devtools";
 
 import Header from "../components/Header";
 
@@ -40,17 +38,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <body>
         <Header />
         {children}
-        <TanStackDevtools
-          config={{
-            position: "bottom-left",
-          }}
-          plugins={[
-            {
-              name: "Tanstack Router",
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-          ]}
-        />
         <Scripts />
       </body>
     </html>
--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart({
-      customViteReactPlugin: true,
+      target: "cloudflare-module",
     }),
     viteReact(),
   ],
